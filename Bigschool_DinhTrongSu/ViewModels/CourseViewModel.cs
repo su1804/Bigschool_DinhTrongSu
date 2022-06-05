ba6b1a5ce59791d90816e64e5f4e6db89fd32dfd
SU <dinhsu180401@gmail.com>
2022-06-05 21:04:23 +0700

    cap nhat

--- Bigschool_DinhTrongSu/ViewModels/CourseViewModel.cs
+++ Bigschool_DinhTrongSu/ViewModels/CourseViewModel.cs
@@ -15,6 +15,7 @@ namespace Bigschool_DinhTrongSu.ViewModels
         [FutureDate]
         public string Date { get; set; }
         [Required]
+        [ValidTime]
         public string Time { get; set; }
         [Required]
         public byte Category { get; set; }
