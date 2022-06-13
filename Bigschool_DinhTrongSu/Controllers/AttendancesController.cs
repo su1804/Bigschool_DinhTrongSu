49a9a7c60b627189634f31d10e8a94350f939a08
SU <dinhsu180401@gmail.com>
2022-06-13 19:09:29 +0700

    cap nhap 3

---
+++ Bigschool_DinhTrongSu/Controllers/AttendancesController.cs
@@ -0,0 +1,37 @@
+﻿using Bigschool_DinhTrongSu.DTOs;
+using Bigschool_DinhTrongSu.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Bigschool_DinhTrongSu.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbcontext;
+        public AttendancesController()
+        {
+            _dbcontext = new ApplicationDbContext();
+        }
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto attendanceDto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbcontext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == attendanceDto.CourseId))
+                return BadRequest("the Attendance lready exists!");
+            var attendance = new Attendance
+            {
+                CourseId = attendanceDto.CourseId,
+                AttendeeId = userId
+            };
+            _dbcontext.Attendances.Add(attendance);
+            _dbcontext.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ Bigschool_DinhTrongSu/Controllers/FollowingsCotroller.cs
@@ -0,0 +1,35 @@
+﻿using Bigschool_DinhTrongSu.DTOs;
+using Bigschool_DinhTrongSu.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Bigschool_DinhTrongSu.Controllers
+{
+    public class FollowingsCotroller : ApiController
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public FollowingsCotroller()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        [HttpPost]
+        public IHttpActionResult Follow(FollowingDto followingDto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Followings.Any(f => f.FollowerId == userId && f.FollowerId == followingDto.FolloweeId))
+                return BadRequest("Following already exists");
+            var folowing = new Following
+            {
+                FollowerId = userId,
+                FolloweeId = followingDto.FolloweeId
+            };
+            _dbContext.Followings.Add(folowing);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+    }
+}
